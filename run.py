bb9502befd464732f1ae8611880682615f7c3192
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-01-02 13:33:13 +0700

    update

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import os
 # job_name = config['job']
 # py_file_names = ','.join(config['py_files'])
 # file_names = ','.join(config['files'])
-os.system("sudo spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 videojob.py --files /yolov5s.pt --py-files /yolov5.zip")
+os.system("sudo spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 videojob.py --files /yolov5s.pt --py-files /models.zip, /utils.zip")
         #   + job_name +
         #   " --py-files "+py_file_names+" "
         #   "--files "+file_names)
