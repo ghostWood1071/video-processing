c861bc1a7ff9b4934ea7e5a8343113a42acedb53
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-01-02 22:54:30 +0700

    update

--- producer.py
+++ producer.py
@@ -37,7 +37,7 @@ def publish_camera(topic, video):
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        topic_name = "video4"  # sys.argv[1]
+        topic_name = "video5"  # sys.argv[1]
         video_path = "c"  # sys.argv[2]
         publish_camera(topic_name, video_path)
     else:
