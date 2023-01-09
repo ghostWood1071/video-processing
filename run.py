e8d641843f19374d5dd7ce447812f90df4d28eb9
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-01-09 20:16:50 +0700

    update

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ import os
 # py_file_names = ','.join(config['py_files'])
 # file_names = ','.join(config['files'])
 os.system(
-"sudo spark-submit" +
+"sudo spark-submit " +
 "--packages" + 
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0" +
-        "com.hortonworks:shc-core:1.1.1-2.1-s_2.11"+ 
-"videojob.py" + 
-"--repositories http://repo.hortonworks.com/content/groups/public/" +
-"--files /yolov5s.pt" 
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 " +
+        "com.hortonworks:shc-core:1.1.1-2.1-s_2.11 "+ 
+"videojob.py " + 
+"--repositories http://repo.hortonworks.com/content/groups/public/ " +
+"--files /yolov5s.pt " 
 "--py-files /models.zip, /utils.zip")
         #   + job_name +
         #   " --py-files "+py_file_names+" "
