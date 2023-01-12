d40ffaeff00d7b7424f1f96aba84a7820a151936
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-01-13 01:44:58 +0700

    update

--- videojob.py
+++ videojob.py
@@ -7,7 +7,8 @@ from datetime import datetime
 from uuid import uuid4
 import torch
 from custom_sink import WriteHbaseRow, process_batch
-
+from happybase import *
+from typing import *
 
 
 
@@ -88,6 +89,33 @@ data_streaming_df = streaming_df.select(col('value').cast('string').name('value'
                                         col('frame_id'), 
                                         col('name'), 
                                         col('frame'))
+
+class WriteHbaseRow:
+    def __init__(self) -> None:
+        self.conn:Connection = Connection(host='10.0.2.195', port=9090, autoconnect=False)
+        self.partition_id = None
+        self.epoch_id = None
+
+    def open(self, partition_id, epoch_id):
+        self.partition_id = partition_id
+        self.epoch_id = epoch_id
+        self.conn.open()
+
+    def process(self, row):
+        table:Table = self.conn.table('video-processing')
+        data:Dict[bytes, bytes] = {
+            b'video:video_id': row['video_id'],
+            b'video:segment_id': row['segment_id'],
+            b'video:frame_id': row['frame_id'],
+            b'object:name': row['name']
+        }
+        table.put(f'{self.epoch_id}-{self.partition_id}-{row["key"]}', data)
+        self.conn.close()
+
+    def close(self, err):
+        self.conn.close()
+        print(err)
+
 
 query = data_streaming_df.writeStream\
 .foreach(WriteHbaseRow)\
