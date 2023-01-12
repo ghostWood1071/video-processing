5ac26d22e87fb63b747e76b4aec2ec844c389be8
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-01-13 01:58:15 +0700

    update

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import os
 # py_file_names = ','.join(config['py_files'])
 # file_names = ','.join(config['files'])
 os.system(
-"sudo spark-submit " +
+"spark-submit " +
 "--packages " + 
         "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 " +
 "videojob.py " + 
