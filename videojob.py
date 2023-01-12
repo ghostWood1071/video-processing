bf2d8b58fe5fc65ded60cfdab59d235e6d85d17b
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-01-13 02:04:05 +0700

    update

--- videojob.py
+++ videojob.py
@@ -19,9 +19,6 @@ session = SparkSession.builder\
 context = session.sparkContext 
 context.setLogLevel("WARN")
 
-context.addPyFile("models.zip")
-context.addPyFile("utils.zip")
-context.addFile("yolov5s.pt")
 # define config info
 host = "192.168.56.7:9092,192.168.56.8:9093"
 stream_format = "kafka"
