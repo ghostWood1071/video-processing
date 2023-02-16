a4a2f0a8c94ad769d89cd8b8d757831a7837e85a
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-02-16 23:44:24 +0700

    change consumer to test

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,4 +1,4 @@
 from kafka.consumer import KafkaConsumer
 consumer = KafkaConsumer('video', bootstrap_servers=["192.168.100.124:9092", "192.168.100.125:9093"])
 for m in consumer:
-    print(m)
+    print("received")
