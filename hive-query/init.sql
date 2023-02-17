96c4b392eba55be0a7701a87028c04e715af0fb5
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2023-02-17 08:57:49 +0700

    add hive sql init

--- hive-query/init.sql
+++ hive-query/init.sql
@@ -8,7 +8,6 @@ frame STRING,
 name STRING)
 STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
 WITH SERDEPROPERTIES 
-('hbase.columns.mapping' = 
-':key, video:video_id, video:segment_id, video:send_time, video:frame_id, video:frame, object:name',)
+('hbase.columns.mapping' = ':key, video:video_id, video:segment_id, video:send_time, video:frame_id, video:frame, object:name')
 TBLPROPERTIES ('hbase.table.name' = 'video-processing');
 
