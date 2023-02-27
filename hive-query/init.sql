f56f588b7ecff26a8a709fc935bc210c564331e8
ghostWood1071 <thinhquangshin@gmail.com>
2023-02-27 14:36:40 +0700

    update hive table

--- hive-query/init.sql
+++ hive-query/init.sql
@@ -1,13 +1,44 @@
-CREATE EXTERNAL TABLE video_processing
+CREATE EXTERNAL TABLE videos
+(rowkey STRING, 
+location STRING, 
+quantity INT)
+STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+WITH SERDEPROPERTIES 
+('hbase.columns.mapping' = ':key, info:location, info:quantity')
+TBLPROPERTIES ('hbase.table.name' = 'cameras');
+
+
+CREATE EXTERNAL TABLE segments
+(rowkey STRING, 
+video_id STRING, 
+url STRING, 
+time_start FLOAT, 
+time_end FLOAT)
+STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+WITH SERDEPROPERTIES 
+('hbase.columns.mapping' = ':key, video:video_id, video:url, time:time_end, time:time_start')
+TBLPROPERTIES ('hbase.table.name' = 'segments');
+
+CREATE EXTERNAL TABLE trackings
+(rowkey STRING, 
+video_id STRING, 
+segment_id STRING
+frame_id STRING
+name STRING, 
+upper STRING, 
+lower STRING)
+STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+WITH SERDEPROPERTIES 
+('hbase.columns.mapping' = ':key, frame:video_id, frame:segment_id, frame:frame_id, object:name, object:upper, object:lower')
+TBLPROPERTIES ('hbase.table.name' = 'trackings');
+
+CREATE EXTERNAL TABLE frames
 (rowkey STRING, 
 video_id STRING, 
 segment_id STRING, 
-send_time STRING, 
-frame_id STRING, 
-frame STRING,
-name STRING)
+send_time FLOAT, 
+content STRING)
 STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
 WITH SERDEPROPERTIES 
-('hbase.columns.mapping' = ':key, video:video_id, video:segment_id, video:send_time, video:frame_id, video:frame, object:name')
-TBLPROPERTIES ('hbase.table.name' = 'video-processing');
-
+('hbase.columns.mapping' = ':key, video:video_id, video:url, time:time_end, time:time_start')
+TBLPROPERTIES ('hbase.table.name' = 'frames');
